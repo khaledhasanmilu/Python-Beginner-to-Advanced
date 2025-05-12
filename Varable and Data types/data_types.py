12cbb3a25e236ff430eb0f3f154f1b2b79a9fb49
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2025-05-12 12:24:58 +0600

    Create data_types.py

---
+++ Varable and Data types/data_types.py
@@ -0,0 +1,59 @@
+# Demonstrating all main Python data types
+
+# Integer
+a = 10
+
+# Float
+b = 3.14
+
+# String
+c = "Hello, Python!"
+
+# Boolean
+d = True
+
+# List
+e = [1, 2, 3, 4]
+
+# Tuple
+f = (5, 6, 7)
+
+# Set
+g = {8, 9, 10}
+
+# Dictionary
+h = {"name": "Khalid", "age": 21}
+
+# NoneType
+i = None
+
+# Complex number
+j = 2 + 3j
+
+# Bytes
+k = b"hello"
+
+# Bytearray
+l = bytearray(b"hello")
+
+# Range
+m = range(5)
+
+# Frozenset
+n = frozenset([1, 2, 3])
+
+# Printing the types of each variable
+print("a:", a, "=>", type(a))
+print("b:", b, "=>", type(b))
+print("c:", c, "=>", type(c))
+print("d:", d, "=>", type(d))
+print("e:", e, "=>", type(e))
+print("f:", f, "=>", type(f))
+print("g:", g, "=>", type(g))
+print("h:", h, "=>", type(h))
+print("i:", i, "=>", type(i))
+print("j:", j, "=>", type(j))
+print("k:", k, "=>", type(k))
+print("l:", l, "=>", type(l))
+print("m:", list(m), "=>", type(m))
+print("n:", n, "=>", type(n))
