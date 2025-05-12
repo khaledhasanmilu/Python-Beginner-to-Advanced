c938786cce9b27a0b38a09738884a4478cbc9625
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2025-05-12 12:25:30 +0600

    Add files via upload

---
+++ Varable and Data types/variable.py
@@ -0,0 +1,45 @@
+# Integer
+age = 21
+
+# Float
+height = 5.9
+
+# String
+name = "Khalid"
+
+# Boolean
+is_student = True
+
+# List
+hobbies = ["coding", "reading", "football"]
+
+# Tuple
+coordinates = (23.8103, 90.4125)
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+# Dictionary
+student_info = {
+    "name": "Khalid",
+    "age": 21,
+    "dept": "CSE"
+}
+
+# NoneType
+future_job = None
+
+# Complex number
+z = 4 + 3j
+
+# Print all variables
+print("Integer:", age)
+print("Float:", height)
+print("String:", name)
+print("Boolean:", is_student)
+print("List:", hobbies)
+print("Tuple:", coordinates)
+print("Set:", unique_numbers)
+print("Dictionary:", student_info)
+print("NoneType:", future_job)
+print("Complex Number:", z)
