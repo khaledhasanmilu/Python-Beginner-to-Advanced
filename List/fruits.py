9240a28757f567708f16303029e36c7e9070a06d
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2025-05-15 01:32:30 +0600

    Add files via upload

---
+++ List/fruits.py
@@ -0,0 +1,12 @@
+fruits = []
+f1 = input("Enter Fruit name: ")
+fruits.append(f1)
+f2 = input("Enter Fruit name: ")
+fruits.append(f2)
+f3 = input("Enter Fruit name: ")
+fruits.append(f3)
+print(fruits)
+print(type(fruits))
+
+l = [1,2,3,4]
+print(sum(l)) #sum
---
+++ List/palindrome.py
@@ -0,0 +1,8 @@
+num1 = ["m","a","m"]
+num2 = num1.copy()
+print(num2)
+num2.reverse()
+if(num2==num1):
+  print("Palindrome")
+else:
+  print("Not Palindrome")  
