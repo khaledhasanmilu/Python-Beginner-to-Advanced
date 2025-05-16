b3e051bc5fff3bfacc7e3e7c4f0b82456461ea26
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2025-05-16 13:25:09 +0600

    Add files via upload

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,10 @@
+dict = {
+   "name":"khaled",
+    "cgpa": 3.69,
+    "marks": [12,24,53],
+    "khaled":("um","ju")
+}
+print(dict["name"])
+print(dict["marks"])
+print(dict["khaled"])
+print(type(dict))
